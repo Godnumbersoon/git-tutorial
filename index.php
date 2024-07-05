7b6b9bb955fd9cd6454c8c1fe6ad2cf784631bd3
Godnumbersoon <6611850014@mut.ac.th>
2024-07-05 17:11:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,3 +7,11 @@ echo "<h1>My repository</h1>"
 echo "<h1>My readadadadadpositorasdy</h1>" 
 echo "<h1>My resadadadpadadository</h1>" 
 ?>
+<?php 
+echo "<h1>My readadadadadpositorasdy</h1>" 
+echo "<h1>My resadadadpadadository</h1>" 
+?>
+<?php 
+echo "<h1>My readadadadadpositorasdy</h1>" 
+echo "<h1>My resadadadpadadository</h1>" 
+?>
